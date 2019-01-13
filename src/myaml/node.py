c7274623a3a1164af8aef298bf905bfb8e934a17
Opemipo Ogunkola <ogunks@live.com>
2019-01-13 21:06:05 +0000

    to_string for MappingNode's done

--- src/myaml/node.py
+++ src/myaml/node.py
@@ -5,14 +5,27 @@ import typing
 class Node(abc.ABC):
 
     regex = None
+    nativeType = None
 
     @abc.abstractclassmethod
     def from_string(cls, string: str) -> 'Node':
         pass
 
     @abc.abstractmethod
-    def to_object(self) -> typing.Any:
+    def to_object(self) -> typing.Union[str, dict, list]:
         pass
+
+    @abc.abstractclassmethod
+    def from_object(cls, obj: typing.Union[str, dict, list]) -> 'Node':
+        pass
+
+    @abc.abstractmethod
+    def to_string(self) -> str:
+        pass
+
+    @classmethod
+    def match_object(cls, obj):
+        return isinstance(obj, cls.nativeType)
 
     @classmethod
     def match(cls, string) -> bool:
--- src/tests/test_myaml.py
+++ src/tests/test_myaml.py
@@ -54,3 +54,56 @@ key4: value
     ])
     def test_simple_parsing(self, string, expected):
         self.assertEqual(myaml.parse(string=string), expected)
+
+
+
+class TestDump(unittest.TestCase):
+
+    @parameterized.expand([
+        ('''blah: value''', {'blah': 'value'}
+        ),
+#         ('''key:
+#     blah: value''', {'key': {'blah': 'value'}}
+#         ),
+#         ('''-   -   blah''', [['        blah']]
+#         ),
+#         ('''
+# key:
+# key2:
+#     ''', {'key': '', 'key2': ''}
+#         ),
+#         ('''
+# key: value
+#     ''', {'key': 'value'}
+#         ),
+#         ('''
+# key: value
+# key2: value2
+#     ''', {'key': 'value', 'key2': 'value2'}
+#         ),
+#         ('''
+# key: value
+# key2: value2
+#     ''', {'key': 'value', 'key2': 'value2'}
+#         ),
+#         ('''
+# key: value
+# key2:
+#     key3: value
+# key4: value
+#     ''', {'key': 'value', 'key2': {'key3': 'value'}, 'key4': 'value'}
+#         ),
+#         ('''
+# - key: value
+# - key2:
+#     key3: value
+# - value
+#     ''', [
+#                 {'key': 'value'},
+#                 {'key2': {'key3': 'value'}},
+#                 '  value',
+#             ]
+#         ),
+    ])
+    def test_simple_dumping(self, expected, obj):
+        self.assertEqual(myaml.dump(obj=obj), expected)
