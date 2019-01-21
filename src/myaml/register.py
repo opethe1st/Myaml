498396c28cef5d3c658e04a0e52830e5a072099d
Opemipo Ogunkola <ogunks@live.com>
2019-01-21 15:42:01 +0000

    add register explicitly to the __init__.py

--- src/myaml/register.py
+++
@@ -1,9 +0,0 @@
-from .mapping_node import MappingNode
-from .node_registry import nodeRegistry
-from .scalar_node import ScalarNode
-from .sequence_node import SequenceNode
-
-
-nodeRegistry.register(MappingNode)
-nodeRegistry.register(SequenceNode)
-nodeRegistry.register(ScalarNode)
